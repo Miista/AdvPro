cd226bb14b10779220cc0b821fd59347e1105b02
Søren Palmund <palmund@gmail.com>
2016-02-26 10:40:48 +0100

    Ex1: add Haskell implementation of compose

--- Exercise 1/src/power.hs
+++ Exercise 1/src/power.hs
@@ -6,3 +6,12 @@ power x n
    | n < 0 = 1 / (power x $ abs n)
    | even n = mult (power x $ div n 2) $ power x $ div n 2
    | otherwise = mult x $ power x $ n-1
+
+c1 :: Int -> Int
+c1 a = a+1
+
+c2 :: Int -> Int
+c2 a = a*2
+
+compose :: (a -> b) -> (b -> c) -> a -> c
+compose f g = \a -> g (f a)
